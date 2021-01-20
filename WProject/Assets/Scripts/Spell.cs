a42f62d76871bdcd02e00c957f9089fee23595f3
Gen0902 <50872015+Gen0902@users.noreply.github.com>
2021-01-20 15:54:25 +0100

    Class skeletons

---
+++ WProject/Assets/Scripts/Spell.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Spell
+{
+    public static void StrongAttack()
+    {
+
+    }
+
+    public static void Heal()
+    {
+
+    }
+
+    public static void Shield()
+    {
+
+    }
+
+    public static void BlockHeal()
+    {
+
+    }
+
+    public static void BlockReflect()
+    {
+
+    }
+
+    public static void LifeSteal()
+    {
+
+    }
+
+    public static void ShieldAttack()
+    {
+
+    }
+
+    public static void Sacrifice()
+    {
+
+    }
+
+    public static void Armor()
+    {
+
+    }
+}
