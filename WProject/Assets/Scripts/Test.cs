0857c52addaeeca1f831b9b4376c2b963ef4b3e3
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-01-20 09:58:14 +0100

    Created Unity project and added EmguCV version 4.4.XXX to assembly

---
+++ WProject/Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
